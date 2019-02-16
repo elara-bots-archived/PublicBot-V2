e030c9b1bcab12ccb4ee84a1afeefed944acb969
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-02-15 18:52:44 -0800

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,4 +2,9 @@ const Discord = require('discord.js');
 module.exports.run = (bot, guild) => {
     require('../util/playing.js')(bot)
  console.log(`I have been removed from ${guild.name} with ${guild.memberCount} members!`)
+//     let e = new Discord.RichEmbed()
+//     .setAuthor(bot.user.tag, bot.user.displayAvatarURL)
+//     .setColor(`#FF000`)
+//     .setDescription(`I have been removed from ${guild.name} with ${guild.memberCount} members!`);
+//     return bot.channels.get("-- LOG CHANNEL ID --").send(e)
 }
