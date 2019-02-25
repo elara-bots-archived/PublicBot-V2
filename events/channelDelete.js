8c23217be3e9d3b60b9563383c15ccbdf4d8ec57
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-02-25 08:36:45 -0800

    Update channelDelete.js

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,13 +1,30 @@
 const Discord = require('discord.js');
-module.exports.run = (bot, channel) => {
+module.exports.run = async (bot, channel) => {
     let guild = channel.guild;
     let modlogs = guild.channels.find(c => c.name === "modlogs");
     if (!modlogs) return;
-    let botembed = new Discord.RichEmbed()
-        .setColor("#FF0000")
-        .setAuthor('Channel Deleted', channel.guild.iconURL)
-        .setFooter(`ID: ${channel.id}`)
-        .setTimestamp()
-        .setDescription(`_ _►Name **${channel.name}**\n ►Type **${channel.type}**\n ►ID ${channel.id}\n ►Position ${channel.position}`)
-    modlogs.send(botembed);
+    let e = new Discord.RichEmbed()
+    if (channel.guild.members.get(bot.user.id).permissions.has("VIEW_AUDIT_LOG")) {
+        let who = await channel.guild.fetchAuditLogs().then(audit => audit.entries.first().executor)
+        e.setDescription(`
+        **Name: **${channel} (${channel.name})
+        **Type: **${channel.type}
+        **ID: **${channel.id}
+        **Deleted By: **${who}`)
+        e.setColor("#FF0000")
+        e.setAuthor('Channel Deleted', channel.guild.iconURL)
+        e.setFooter(`ID: ${channel.id}`)
+        e.setTimestamp()
+       return modlogs.send(e)
+    } else {
+        e.setDescription(`
+        **Name: **${channel} (${channel.name})
+        **Type: **${channel.type}
+        **ID: **${channel.id}`)
+        e.setColor("#FF0000")
+        e.setAuthor('Channel Deleted', channel.guild.iconURL)
+        e.setFooter(`ID: ${channel.id}`)
+        e.setTimestamp()
+        return modlogs.send(e)
+    }
 }
