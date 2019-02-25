aa431a43dfa955d49685c6db9f60d1b164260ebb
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-02-25 08:39:58 -0800

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -3,11 +3,29 @@ module.exports.run = async (bot, channel) => {
     if (channel.type === "dm") return undefined;
     let modlogs = channel.guild.channels.find(c => c.name === "modlogs");
     if (!modlogs) return;
-    let botembed = new Discord.RichEmbed()
-        .setColor("#FF000")
-        .setAuthor('Channel Created', channel.guild.iconURL)
-        .setFooter(`ID: ${channel.id}`)
-        .setTimestamp()
-        .setDescription(`_ _►Name<#${channel.id}> (**${channel.name}**) \n ►Type **${channel.type}** \n ►ID **${channel.id}**`)
-    await modlogs.send(botembed);
+   let e = new Discord.RichEmbed()
+    if (channel.guild.members.get(bot.user.id).permissions.has("VIEW_AUDIT_LOG")) {
+        let logs = await channel.guild.fetchAuditLogs({ type: "CHANNEL_CREATE" });
+        let who = await logs.entries.first().executor
+        e.setDescription(`
+        **Name: **${channel} (${channel.name})
+        **Type: **${channel.type}
+        **ID: **${channel.id}
+        **Created By: **${who}`)
+        e.setColor("#FF000")
+        e.setAuthor('Channel Created', channel.guild.iconURL)
+        e.setFooter(`ID: ${channel.id}`)
+        e.setTimestamp()
+       return modlogs.send(e)
+    } else {
+        e.setDescription(`
+        **Name: **${channel} (${channel.name})
+        **Type: **${channel.type}
+        **ID: **${channel.id}`)
+        e.setColor("#FF000")
+        e.setAuthor('Channel Created', channel.guild.iconURL)
+        e.setFooter(`ID: ${channel.id}`)
+        e.setTimestamp()
+        return modlogs.send(e)
+    }
 }
