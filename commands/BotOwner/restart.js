4a5e2a55d2fe59eeeee989f880da9b60b4e97d38
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-04-18 18:47:15 -0700

    Update restart.js

--- commands/BotOwner/restart.js
+++ commands/BotOwner/restart.js
@@ -13,11 +13,9 @@ module.exports = class RestartCommand extends Command {
         })
     }
     async run(message){
-        let embed = new Discord.RichEmbed()
-            .setColor(`RANDOM`)
-            .setTimestamp()
-            .setFooter(`Restarted At`)
-            .setDescription(`${message.author} Restarted the bot`)
-        this.client.channels.get("468372950266150916").send(embed).then(message.react('426956230582599690')).then(this.client.destroy().then(this.client.login(config.token)).then( await message.say(`Successfully Restarted. ${this.client.emojis.get('476629550797684736')}`)))
+    message.channel.send(`Restarting....`)
+    setTimeout(async () => {   
+    this.client.destroy().then(this.client.login(config.token))
+    }, 2000)
     }
 }
