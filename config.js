bf444bdb9196a938a47968caefd7072aa4a26281
Elara <43923163+Elara-Discord-Bots@users.noreply.github.com>
2019-05-25 00:06:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "YOUR BOT TOKEN HERE",
-    fortnite: "YOUR FORTNITE API KEY HERE",
-    youtubeapi: "YOUR YOUTUBE API KEY HERE",
-    giphy: "YOUR GIPHY API KEY HERE",
-    twitch: "YOUR TWITCH API KEY HERE",
+    token: process.env.token,
+    fortnite: process.env.fortnite,
+    youtubeapi: process.env.yt,
+    giphy: process.env.gif,
+    twitch: process.env.twitch,
     
 }
